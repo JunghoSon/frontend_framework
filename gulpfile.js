4004e85e877979d582e5cc20533a5bb75dda0547
JunghoSon <jhson0819@gmail.com>
2015-08-03 16:02:48 +0900

    set path

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,9 @@ gulp.task('clean',function(){
 	del([
 		config.path.html.dest+'*',
 		config.path.css.dest+'*',
-		config.path.js.dest+'*'
+		config.path.js.dest.libs+'*',
+		config.path.js.dest.plugin+'*',
+		config.path.js.dest.app+'*',
 		//config.path.images.dest+'*'
 	]);
 });
@@ -51,19 +53,43 @@ gulp.task('styles',function(){
 		.pipe(gulp.dest(config.path.css.dest));
 });
 
-/* script [ jshint, concat, uglify, rename ] */
-gulp.task('scripts',function(){
+/* scripts */
+gulp.task('scripts',['js:libs','js:plugin','js:app']);
+
+/* js:libs [ jshint, concat, uglify, rename ] */
+gulp.task('js:libs',function(){
+	gulp.src(config.path.js.src.libs)
+		.pipe(gulpif(config.lint.scripts.libs, jshint()))
+		.pipe(gulpif(config.lint.scripts.libs, jshint.reporter('jshint-stylish')))
+		.pipe(gulpif(config.concat.scripts.libs,concat(config.path.js.result)))
+		.pipe(gulpif(config.uglify.scripts.libs, uglify(config.uglify_option)))
+		.pipe(gulpif(config.rename.scripts.libs, rename(config.rename_option)))
+		.pipe(gulp.dest(config.path.js.dest.libs));
+});
+
+/* js:plugin [ jshint, concat, uglify, rename ] */
+gulp.task('js:plugin',function(){
+	gulp.src(config.path.js.src.plugin)
+		.pipe(gulpif(config.lint.scripts.plugin, jshint()))
+		.pipe(gulpif(config.lint.scripts.plugin, jshint.reporter('jshint-stylish')))
+		.pipe(gulpif(config.concat.scripts.plugin,concat(config.path.js.result)))
+		.pipe(gulpif(config.uglify.scripts.plugin, uglify(config.uglify_option)))
+		.pipe(gulpif(config.rename.scripts.plugin, rename(config.rename_option)))
+		.pipe(gulp.dest(config.path.js.dest.plugin));
+});
+
+/* js:app [ jshint, concat, uglify, rename ] */
+gulp.task('js:app',function(){
 	gulp.src([
-			config.path.js.src+'libs/jquery-1.11.3.min.js',
-			config.path.js.src+'app/app.js',
-			config.path.js.src + '**/*.js'
+			config.path.js.src.app+'app.js',
+			config.path.js.src.app+'**/*.js',
 		])
-		.pipe(gulpif(config.lint.scripts, jshint()))
-		.pipe(gulpif(config.lint.scripts, jshint.reporter('jshint-stylish')))
-		.pipe(gulpif(config.concat.scripts,concat(config.path.js.result)))
-		.pipe(gulpif(config.uglify.scripts, uglify(config.uglify_option)))
-		.pipe(gulpif(config.rename.scripts, rename(config.rename_option)))
-		.pipe(gulp.dest(config.path.js.dest));
+		.pipe(gulpif(config.lint.scripts.app, jshint()))
+		.pipe(gulpif(config.lint.scripts.app, jshint.reporter('jshint-stylish')))
+		.pipe(gulpif(config.concat.scripts.app,concat(config.path.js.result)))
+		.pipe(gulpif(config.uglify.scripts.app, uglify(config.uglify_option)))
+		.pipe(gulpif(config.rename.scripts.app, rename(config.rename_option)))
+		.pipe(gulp.dest(config.path.js.dest.app));
 });
 
 /* images 
@@ -84,6 +110,6 @@ gulp.task('watch',function(){
 	gulp.watch(config.path.js.src+'**/*.js',['scripts']);
 	gulp.watch(config.path.css.src,['styles']);
 	gulp.watch(config.path.html.src,['html']);
-	gulp.watch(config.path.images.src,['images']);
+	//gulp.watch(config.path.images.src,['images']);
 	gulp.watch(config.path.dist+'**').on('change',livereload.changed);
 });
